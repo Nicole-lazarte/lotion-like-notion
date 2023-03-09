0985b5a8bdb3f67834cf5548e1f0655da2f9fc91
Nicole-lazarte <nicole.lazarte@ucalgary.ca>
2023-03-09 16:09:31 -0700

    new changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import lotionLayout from "./lotionLayout.js";
+/*import preview from "./preview.js";
+import editing from "./editing.js";
+import emptyNote from "./emptyNote.js";
+*/
+
 function App() {
-  return <h1>Lotion</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<lotionLayout />} exact>
+          <Route path="/" element={<emptyNote />} exact />
+          <Route path="/preview/:id" element={<preview />} />
+          <Route path="/editing/:id" element={<editing />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
